ca6b7c1d83d84592612afe54a2a53fadc6fe4638
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-09 16:04:14 +0100

    completato

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+// ESERCIZIO 1
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+//----------------------------------------------------
+var studente = {
+  "nome": "Mel",
+  "cognome": "Gibson",
+  "eta": 64
+};
+
+for (var key in studente) {
+  console.log(key + " :*" + studente[key] + "*");
+}
+//----------------------------------------------------------
+
+//ESERCIZIO 2
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+//----------------------------------------------------
+
+var listaStudenti = [
+  {
+    "nome": "Sylvester",
+    "cognome": "Stallone",
+    "eta": 74
+  },
+  {
+    "nome": "Arnold",
+    "cognome": "Schwarzenegger",
+    "eta": 73
+  },
+  {
+    "nome": "Jean-Claude",
+    "cognome": "Van Damme",
+    "eta": 60
+  }
+];
+
+
+for (var i = 0; i < listaStudenti.length; i++) {
+  console.log("Nome: *" + listaStudenti[i].nome + "* " + "Cognome: *" + listaStudenti[i].cognome + "*");
+}
+//----------------------------------------------------
+
+
+//ESERCIZIO 3
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+// Possiamo fare anche sullo stesso file o comunque stessa cartella, se ci pensate può essere una sequenza sullo stesso ex.
+// Anche nel punto 2 gli oggetti studenti che stasnno nell’array hanno almeno le 3 proprietà come in quello iniziale.
+
+var userName = prompt("Inserisci il tuo nome");
+var userSurname = prompt("Inserisci il tuo cognome");
+var userEta = parseInt(prompt("Inserisci la tua età"));
+
+
+function userStudente(name, surname, age) {
+  this.nome = name;
+  this.cognome = surname;
+  this.eta = age;
+}
+
+var nuovoStudente = new userStudente(userName, userSurname, userEta);
+
+console.log("Ciao " + userName + ", benvenuto in questa splendida classe");
+
+listaStudenti.push(nuovoStudente);
+
+for (var i = 0; i < listaStudenti.length; i++) {
+  console.log("Nome: *" + listaStudenti[i].nome + "* " + "Cognome: *" + listaStudenti[i].cognome + "*");
+}
